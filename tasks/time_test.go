4cf6c0098640661e91b8446b9bc4a368f5b66ea0
Kevin <kevin@mcmullin.one>
2021-09-08 20:29:23 +0700

    added datetime parsing test

--- tasks/time_test.go
+++ tasks/time_test.go
@@ -83,5 +83,79 @@ func TestTimeParse(t *testing.T) {
 }
 
 func TestDatetimeParse(t *testing.T) {
+	
+	var tests = []struct{
+		str		string
+		want		[7]int
+		timezone	*time.Location
+	}{
+		{
+			str: "2021-05-21T12:00:00+06:00",
+			want: [7]int{ 2021, 5, 21, 12, 0, 0, 0 },
+			timezone: time.FixedZone("UTC+6", 6*60*60),
+		},
+		{
+			str: "2022-08-01T16:30:30+12:00",
+			want: [7]int{ 2022, 8, 1, 16, 30, 30, 0},
+			timezone: time.FixedZone("UTC+12", 12*60*60),
+		},
+	}
+
+	for _, tt := range tests {
+		failed := false
+		datetimeString := tasks.Datetime(tt.str)
+		
+		parsedDatetime, err := datetimeString.Parse()
+
+		if err != nil {
+			t.Errorf("failed to parse time string: %v", err.Error())
+		}
+
+		parsedDatetime = parsedDatetime.In(tt.timezone)
+
+		if got := parsedDatetime.Year(); got != tt.want[0] {
+			t.Errorf("year: want %v got %v for input %v", tt.want[0], got, tt.str)
+			failed = true
+		}
+		if got := parsedDatetime.Month(); int(got) != tt.want[1] {
+			t.Errorf("month: want %v got %v for input %v", tt.want[1], got, tt.str)
+			failed = true
+		}
+		if got := parsedDatetime.Day(); got != tt.want[2] {
+			t.Errorf("day: want %v got %v for input %v", tt.want[2], got, tt.str)
+			failed = true
+		}
+		if got := parsedDatetime.Hour(); got != tt.want[3] {
+			t.Errorf("hour: want %v got %v for input %v", tt.want[3], got, tt.str)
+			failed = true
+		}
+		if got := parsedDatetime.Minute(); got != tt.want[4] {
+			t.Errorf("minute: want %v got %v for input %v", tt.want[4], got, tt.str)
+			failed = true
+		}
+		if got := parsedDatetime.Second(); got != tt.want[5] {
+			t.Errorf("second: want %v got %v for input %v", tt.want[5], got, tt.str)
+			failed = true
+		}
+		if got := parsedDatetime.Nanosecond(); got != tt.want[6] {
+			t.Errorf("nseconds: want %v got %v for input %v", tt.want[6], got, tt.str)
+			failed = true
+		}
+
+		convertedTime := parsedDatetime.In(tt.timezone)
+
+		if convertedTime.Format(time.RFC3339) != parsedDatetime.Format(time.RFC3339) {
+			t.Errorf("incorrect timezone: want %v got %v for input %v", convertedTime.Location(), parsedDatetime.Location(), tt.str)
+			failed = true
+		}
+
+		fmt.Printf("timezone: %v and %v\n", parsedDatetime, convertedTime)
+		
+		if failed {
+			fmt.Printf("case failed: %v\n", tt.str)
+		} else {
+			fmt.Printf("case passed: %v\n", tt.str)
+		}
+	}
 
 }
