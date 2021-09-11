fed24aab51d7e96fca96e990d127995374861588
Kevin <kevin@mcmullin.one>
2021-09-11 17:39:03 +0700

    added a graceful shutdown to server

--- main.go
+++ main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+	
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
-	e.Logger.Fatal(e.Start(":8000"))
+	go func () {
+		if err := e.Start(":8000"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatalf("crashed: %v\n", err)
+			log.Panicln("server crashed")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds. 
+	// Use a buffered channel to avoid missing signals as recommended for signal.Notify
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	// block until received interrupt signal
+	<-quit
+	log.Println("starting server shutdown")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		log.Panic("failed to shutdown server properly. check server logs for more info")
+		e.Logger.Fatalf("failed to shutdown: %v\n", err)
+	} else {
+		log.Println("server shutdown down properly")
+	}
+
 }
